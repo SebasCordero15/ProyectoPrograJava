199bed91fa5c3b0cb6934667aa2459f264ddb5ee
Eduin Morales <eduin3221@gmail.com>
2023-10-16 18:49:50 -0600

    Proyecto inicializado

--- ProyectoJava/src/proyectojava/Main.java
+++ ProyectoJava/src/proyectojava/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        System.out.println("Hola mundo Edu");
+        //Project inicializado
     }
     
 }
