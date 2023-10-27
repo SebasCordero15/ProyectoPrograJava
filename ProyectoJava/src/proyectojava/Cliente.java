d371f693f55fb67e40a6922ddc0b795487201901
Kennier <kennier.trejos@gmail.com>
2023-10-27 17:34:20 -0600

    consultar clientes

--- ProyectoJava/src/proyectojava/Cliente.java
+++ ProyectoJava/src/proyectojava/Cliente.java
@@ -1,67 +1,66 @@
 package proyectojava;
-public class Cliente{
+
+public class Cliente {
     private String nombre;
     private String apellidos;
     private String estado;
     private boolean habilitado;
 
-    public Cliente(){
+    public Cliente() {
         this.nombre = "";
         this.apellidos = "";
         this.estado = "";
         this.habilitado = true;
     }
 
-    // Constructor que acepta datos para inicializar un cliente
-    public Cliente(String nombre, String apellidos, String estado, boolean habilitado){
+    public Cliente(String nombre, String apellidos, String estado, boolean habilitado) {
         this.nombre = nombre;
         this.apellidos = apellidos;
         this.estado = estado;
         this.habilitado = habilitado;
     }
 
-    public String getNombre(){
+    public String getNombre() {
         return nombre;
     }
 
-    public void setNombre(String nombre){
+    public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
-    public String getApellidos(){
+    public String getApellidos() {
         return apellidos;
     }
 
-    public void setApellidos(String apellidos){
+    public void setApellidos(String apellidos) {
         this.apellidos = apellidos;
     }
 
-    public String getEstado(){
+    public String getEstado() {
         return estado;
     }
 
-    public void setEstado(String estado){
+    public void setEstado(String estado) {
         this.estado = estado;
     }
 
-    public boolean isHabilitado(){
+    public boolean isHabilitado() {
         return habilitado;
     }
 
-    public void setHabilitado(boolean habilitado){
+    public void setHabilitado(boolean habilitado) {
         this.habilitado = habilitado;
     }
-    
-    public void mostrarInformacion(){
-    System.out.println("Nombre: " + nombre);
+
+    public void mostrarInformacion() {
+        System.out.println("Nombre: " + nombre);
         System.out.println("Apellidos: " + apellidos);
         System.out.println("Estado: " + estado);
-        System.out.println("Habilitado: " + habilitado);   
+        System.out.println("Habilitado: " + habilitado);
     }
-    
-    public static void main(String[]args){
-    Cliente nuevoCliente = new Cliente("", "", "", true);
-    
-    nuevoCliente.mostrarInformacion();
+
+    public void consultarCliente() {
+        System.out.println("Información del Cliente:");
+        mostrarInformacion();
     }
 }
--- ProyectoJava/src/proyectojava/Main.java
+++ ProyectoJava/src/proyectojava/Main.java
@@ -5,7 +5,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
         System.out.println("Hola mundo Edu");
-        //Project inicializado
+        // Project inicializado
+
+        //ejemplo
+        Cliente nuevoCliente = new Cliente("Juan", "Pérez", "Activo", true);
+
+        nuevoCliente.consultarCliente();
     }
-    
 }
