0826d751a7f3ee81ee4e0fd4680ac5e354835402
SebasCordero15 <sebascordero4@gmail.com>
2023-10-27 08:57:26 -0600

    Empleado
    Empleado

---
+++ ProyectoJava/src/proyectojava/Empleado.java
@@ -0,0 +1,64 @@
+package proyectojava;
+public class Empleado {
+    private String Nombre;
+    private String Apellido;
+    private String Usuario;
+    private String Password;
+    private String Estado;
+    
+    public Empleado(){
+        this.Nombre="";
+        this.Apellido="";
+        this.Usuario="";
+        this.Password="";
+        this.Estado="";
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getApellido() {
+        return Apellido;
+    }
+
+    public void setApellido(String Apellido) {
+        this.Apellido = Apellido;
+    }
+
+    public String getUsuario() {
+        return Usuario;
+    }
+
+    public void setUsuario(String Usuario) {
+        this.Usuario = Usuario;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String Password) {
+        this.Password = Password;
+    }
+
+    public String getEstado() {
+        return Estado;
+    }
+
+    public void setEstado(String Estado) {
+        this.Estado = Estado;
+    }
+    public void Informacion(){
+        System.out.println("Nombre: " + Nombre);
+        System.out.println("Apellidos: " + Apellido);
+        System.out.println("USername: " + Usuario);
+        System.out.println("Password: " + Password);
+        System.out.println("Estado: " + Estado);
+    }
+           
+}
