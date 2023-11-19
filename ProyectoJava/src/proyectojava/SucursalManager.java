e552124ed97d36b6d10904b97494804c19820efa
Eduin Morales <46619447+RamzaF@users.noreply.github.com>
2023-11-18 21:41:17 -0600

    Agreagadas funciones para editar e inactivar sucursales - Cambios en el menu

--- ProyectoJava/src/proyectojava/SucursalManager.java
+++ ProyectoJava/src/proyectojava/SucursalManager.java
@@ -1,5 +1,6 @@
 package proyectojava;
 import java.util.ArrayList;
+
 public class SucursalManager {
     private ArrayList<CatalogoSucursal> sucursales;
 
@@ -19,6 +20,16 @@ public class SucursalManager {
         }
         return null;
     }
+    
+    public void editarSucursal(String nombre, String lugar, String horario, String telefono, String correo) {
+   CatalogoSucursal sucursal = this.consultarSucursal(nombre);
+   if (sucursal != null) {
+       sucursal.setLugar(lugar);
+       sucursal.setHorario(horario);
+       sucursal.setTelefono(telefono);
+       sucursal.setCorreo(correo);
+   }
+}
 
     public void inactivarSucursal(String Nombre) {
         CatalogoSucursal sucursal = this.consultarSucursal(Nombre);
